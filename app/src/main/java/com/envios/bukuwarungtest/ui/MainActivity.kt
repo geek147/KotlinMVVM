f25213a75870d4677473a7dfb3a21d5093fd6519
ilham <ilhamaulia.malik@gmail.com>
2020-05-17 08:27:13 +0700

    prevent recreating everytime navigate

--- app/src/main/java/com/envios/bukuwarungtest/ui/MainActivity.kt
+++ app/src/main/java/com/envios/bukuwarungtest/ui/MainActivity.kt
@@ -1,29 +1,62 @@
 package com.envios.bukuwarungtest.ui
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.envios.bukuwarungtest.R
+import com.envios.bukuwarungtest.ui.home.HomeFragment
+import com.envios.bukuwarungtest.ui.profile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    val fragment1: Fragment = HomeFragment()
+    val fragment2: Fragment = ProfileFragment()
+    val fm: FragmentManager = supportFragmentManager
+    var active: Fragment = fragment1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home,
-            R.id.navigation_profile
-        ))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragment2, "2").hide(fragment2).commit()
+        fm.beginTransaction().add(R.id.nav_host_fragment,fragment1, "1").commit()
+
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.navigation_home,
+//            R.id.navigation_profile
+//        ))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
+
+    private val mOnNavigationItemSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        fm.beginTransaction().hide(active).show(fragment1).commit()
+                        active = fragment1
+                        return true
+                    }
+                    R.id.navigation_profile -> {
+                        fm.beginTransaction().hide(active).show(fragment2).commit()
+                        active = fragment2
+                        return true
+                    }
+
+                }
+                return false
+            }
+        }
 }
